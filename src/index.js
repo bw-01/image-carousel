c1a41aec6ec420e0e75f0ef11c08aaecf1f67dd0
BW <17097491+bw-01@users.noreply.github.com>
2024-10-14 12:30:04 +1300

    Added nav control and highlighting and simple page style

--- src/index.js
+++ src/index.js
@@ -1,29 +1,76 @@
 import "./styles.css";
 
+const imageWidthStyle = getComputedStyle(document.documentElement).getPropertyValue(
+  "--image-width"
+);
+const slideWidth = parseInt(imageWidthStyle, 10);
 let xPos = 0;
 let slideCount = 4;
-const slideWidth = 300;
+const interval = 5000;
+let intervalID = setInterval(next, 5000);
 
 function next() {
-  const slidesDiv = document.querySelector(".slides");
   xPos -= slideWidth;
   if (xPos <= -(slideCount * slideWidth)) {
     xPos = 0;
   }
   slidesDiv.style.transform = `translateX(${xPos}px)`;
+  updateNavButton();
 }
 
 function previous() {
-  const slidesDiv = document.querySelector(".slides");
   xPos += slideWidth;
   if (xPos > 0) {
     xPos = -(slideWidth * (slideCount - 1));
   }
   slidesDiv.style.transform = `translateX(${xPos}px)`;
+  updateNavButton();
+}
+
+function updateNavButton() {
+  let index = Math.abs(xPos / slideWidth);
+
+  navButtons.forEach((button) => button.classList.remove("active"));
+
+  const activeButton = document.querySelector(`#btn${index + 1}`);
+  if (activeButton) {
+    activeButton.classList.add("active");
+  }
+}
+
+function resetInterval() {
+  clearInterval(intervalID);
+  intervalID = setInterval(next, interval);
 }
 
 const leftButton = document.querySelector(".left");
 const rightButton = document.querySelector(".right");
 
-leftButton.addEventListener("click", previous);
-rightButton.addEventListener("click", next);
+leftButton.addEventListener("click", () => {
+  previous();
+  resetInterval();
+});
+
+rightButton.addEventListener("click", () => {
+  next();
+  resetInterval();
+});
+
+const navButtons = document.querySelectorAll(".slider button");
+const slidesDiv = document.querySelector(".slides");
+
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {    
+    const buttonID = button.id;
+    const index = parseInt(buttonID.replace("btn", "")) - 1;
+
+    xPos = -(slideWidth * index);
+    slidesDiv.style.transform = `translateX(${xPos}px)`;
+
+    updateNavButton();
+    resetInterval();
+  });
+});
+
+updateNavButton();
+
