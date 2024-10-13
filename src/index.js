52013dcdff77a3aa20563c2cfbffe2d2f63196af
BW <17097491+bw-01@users.noreply.github.com>
2024-10-14 10:47:44 +1300

    Updated to use a background image div which rotates into a frame div. Added scrolling.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+
+let xPos = 0;
+let slideCount = 4;
+const slideWidth = 200;
+
+function next() {
+  const slidesDiv = document.querySelector(".slides");
+  xPos -= 200;
+  if (xPos <= -(slideCount * slideWidth)) {
+    xPos = 0;
+  }
+  slidesDiv.style.transform = `translateX(${xPos}px)`;
+}
+
+function previous() {
+  const slidesDiv = document.querySelector(".slides");
+  xPos += 200;
+  if (xPos > 0) {
+    xPos = -(slideWidth * (slideCount - 1));
+  }
+  slidesDiv.style.transform = `translateX(${xPos}px)`;
+}
+
+const leftButton = document.querySelector(".left");
+const rightButton = document.querySelector(".right");
+
+leftButton.addEventListener("click", () => {
+  previous();
+});
+
+rightButton.addEventListener("click", () => {
+  next();
+});
